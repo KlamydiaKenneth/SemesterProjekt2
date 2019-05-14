d05967c179f639758577ff7efe93eb63c0fd7d29
martin-s-m@hotmail.com <martin-s-m@hotmail.com>
2019-05-14 21:45:04 +0200

    Merge Fixing 0.2
    content and upload should work just fine now with local storage
    added SDU.dk link in the footer

--- SemesterProjekt2/jQuery/contentLoader.js
+++ SemesterProjekt2/jQuery/contentLoader.js
@@ -12,45 +12,53 @@ function loadContent() {
 		converter(this);
 	}
 };
-xmlhttp.open("GET", "js/file_catalog.xml", true);
+xmlhttp.open("GET", "jQuery/file_catalog.xml", true);
 xmlhttp.send();
 }
 function converter(xml) {
-var xmlDoc = xml.responseXML; 
-var x = xmlDoc.getElementsByTagName("ADMIN")[0].getElementsByTagName("FILE");//USERID1 is just a temp name for a user
-var content = "";
-//converts the content of the xml file to HTML
-for (i = 0; i <x.length; i++) {
-	content += "<div class='item'><img class='img' SRC='";
-	content += x[i].getElementsByTagName("IMGSRC")[0].childNodes[0].nodeValue; //Example::  img/SemesterProjektFileIMG.png
-	content += "'><p class='name'><img class='fileIcon' SRC='img/SemesterProjektFileIMG.png'>";
-	content += x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue; //Example::  test_file1.js
-	content += "</p><p class='subject'><img class='subjectIcon' SRC='img/BookIcon.png'>";
-	content += x[i].getElementsByTagName("SUBJECT")[0].childNodes[0].nodeValue; //Example::  Distribueret programmering
-	content += "</p><p class='desc'>";
-	content +=  x[i].getElementsByTagName("DESC")[0].childNodes[0].nodeValue; //Example::  a very descriptive description
-	content += "</p><a href='#DOWNLOADING' class='loginLink'><img class='downloadIcon' SRC='img/DownloadIcon.png'></a></div>";
-}
+	var xmlDoc = xml.responseXML; 
+	var x = xmlDoc.getElementsByTagName("ADMIN")[0].getElementsByTagName("FILE");//USERID1 is just a temp name for a user
+	var content = "";
+	//converts the content of the xml file to HTML
+	for (i = 0; i <x.length; i++) {
+		content += "<div class='item'><img class='img' SRC='";
+		content += x[i].getElementsByTagName("IMGSRC")[0].childNodes[0].nodeValue; //Example::  img/SemesterProjektFileIMG.png
+		content += "'><p class='name'><img class='nameIcon' SRC='img/SemesterProjektFileIMG.png'>";
+		content += x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue; //Example::  test_file1.js
+		content += "</p><p class='subject'><img class='subjectIcon' SRC='img/BookIcon.png'>";
+		content += x[i].getElementsByTagName("SUBJECT")[0].childNodes[0].nodeValue; //Example::  Distribueret programmering
+		content += "</p><p class='desc'>";
+		content +=  x[i].getElementsByTagName("DESC")[0].childNodes[0].nodeValue; //Example::  a very descriptive description
+		content += "</p><a href='#DOWNLOADING' class='loginLink'><img class='downloadIcon' SRC='img/DownloadIcon.png'></a></div>";
+	}
 
-    var localFiles = JSON.parse(localStorage.getItem("localFiles") || "[]");
+		var localFiles = JSON.parse(localStorage.getItem("localFiles") || "[]");
+
+		localFiles.forEach(function(file, index) {
+			
+		console.log("'loading'[" + index + "]: " + file.name);
+		
+		content += "<div class='item'><img class='img' SRC='";
+		content += file.IMGSRC; //Example::  img/SemesterProjektFileIMG.png
+		content += "'><p class='name'><img class='nameIcon' SRC='img/SemesterProjektFileIMG.png'>";
+		content += file.name; //Example::  test_file1.js
+		content += "</p><p class='subject'><img class='subjectIcon' SRC='img/BookIcon.png'>";
+		content += file.Subject; //Example::  Distribueret programmering
+		content += "</p><p class='desc'>";
+		content += file.Description; //Example::  a very descriptive description
+		content += "</p><a href='#DOWNLOADING' class='loginLink'><img class='downloadIcon' SRC='img/DownloadIcon.png'></a></div>";
+	} );
+
+	//Replaces the content of the Main div with the tags made using the XML
+	if(content.length > 0){
+		
+	document.getElementById("main").insertAdjacentHTML('beforeend',content);
+	}
 	
-    localFiles.forEach(function(file, index) {
-    console.log("'loading'[" + index + "]: " + file.name);
-   
- 
-	content += "<div class='item'><img class='img' SRC='";
-	content += file.IMGSRC; //Example::  img/SemesterProjektFileIMG.png
-	content += "'><p class='name'><img class='fileIcon' SRC='img/SemesterProjektFileIMG.png'>";
-	content += file.name; //Example::  test_file1.js
-	content += "</p><p class='subject'><img class='subjectIcon' SRC='img/BookIcon.png'>";
-	content += file.Subject; //Example::  Distribueret programmering
-	content += "</p><p class='desc'>";
-	content += file.Description; //Example::  a very descriptive description
-	content += "</p><a href='#DOWNLOADING' class='loginLink'><img class='downloadIcon' SRC='img/DownloadIcon.png'></a></div>";
-} );
-
-//Replaces the content of the Main div with the tags made using the XML
-document.getElementById("main").innerHTML = content;
+	//preventDefault på download iconerne
+	$('.item .loginLink').click(function(event) {
+		event.preventDefault();
+	});
 }
 
 
